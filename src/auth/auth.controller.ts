6340cc71c57d9c20b033647066224d378e936547
quanngoc <38402405+quanngoc@users.noreply.github.com>
2024-01-06 15:07:18 +0700

    add connect api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Get('connect')
+  connect(): Promise<string> {
+    return new Promise((resolve) => resolve("Hello world"));
+  }
 
   @Post('register')
   register(@Body() dto: AuthDto): Promise<string> {
